da0c5e84aebfa8933bb42efb34b3e683477a8269
ValyaDudchak <dudchak.valya@gmail.com>
2023-04-06 10:36:43 +0300

    task exceptions: modified greeting.cpp

--- 2023/homework/0406-func-ptr-mod/2-greeting.cpp
+++ 2023/homework/0406-func-ptr-mod/2-greeting.cpp
@@ -30,35 +30,74 @@ string FrenchGreeting(string name)
 
 int main()
 {
-  ofstream file("output2.txt");
-
-  cout << "Enter your name: ";
-  string name;
-  cin >> name;
-
-  cout << endl << "Choose language:" << endl;
-  cout << "1 - ukraininan" << endl;
-  cout << "2 - english" << endl;
-  cout << "3 - german" << endl;
-  cout << "4 - french" << endl;
-  int lang;
-  cin >> lang;
-
-  switch(lang)
+  try
   {
-    case 1:
-      file << Greeting(UkrGreeting, name) << endl;
-      break;
-    case 2:
-      file << Greeting(EngGreeting, name) << endl;
-      break;
-    case 3:
-      file << Greeting(GerGreeting, name) << endl;
-      break;
-    case 4:
-      file << Greeting(FrenchGreeting, name) << endl;
-      break;
-    default:
-      cout << "Invalid language";
+    ofstream file("output2.txt");
+
+    if(!file.is_open())
+    {
+      throw runtime_error("can't open file");
+    }
+
+    cout << "Enter your name: ";
+    string name;
+    cin >> name;
+
+    cout << endl << "Choose language:" << endl;
+    cout << "1 - ukraininan" << endl;
+    cout << "2 - english" << endl;
+    cout << "3 - german" << endl;
+    cout << "4 - french" << endl;
+    int lang;
+    cin >> lang;
+
+    switch(lang)
+    {
+      case 1:
+        file << Greeting(UkrGreeting, name) << endl;
+        if(Greeting(UkrGreeting, name) != "З днем народження, " + name + "!")
+        {
+          throw ("invalid result");
+        }
+        break;
+      case 2:
+        file << Greeting(EngGreeting, name) << endl;
+        if(Greeting(EngGreeting, name) != "Happy birthday, " + name + "!")
+        {
+          throw ("invalid result");
+        }
+        break;
+      case 3:
+        file << Greeting(GerGreeting, name) << endl;
+        if(Greeting(GerGreeting, name) != "Alles Gute zum Geburtstag, " + name + "!")
+        {
+          throw ("invalid result");
+        }
+        break;
+      case 4:
+        file << Greeting(FrenchGreeting, name) << endl;
+        if(Greeting(FrenchGreeting, name) != "Joyeux anniversaire, " + name + "!")
+        {
+          throw ("invalid result");
+        }
+        break;
+      default:
+        throw invalid_argument("bad argument: there is no such language in the list");
+    }
+  }
+
+  catch(const invalid_argument &e)
+  {
+    cerr << e.what() << endl;
+  }
+
+  catch(const string &e)
+  {
+    cerr << e << endl;
+  }
+
+  catch(const runtime_error &e)
+  {
+    cerr << e.what() << endl;
   }
 }
